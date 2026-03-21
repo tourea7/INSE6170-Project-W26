ce956a2ab20d1e799f034cb537fae3ba6bba5d51
Aicha Yasmine Toure <toure.a@iugb.edu.ci>
2026-03-21 15:33:20 -0400

    Add IPS monitoring system

--- core/ips.py
+++ core/ips.py
@@ -27,13 +27,13 @@ def get_bandwitdh (device_ip):
 def send_alert(device_ip, current_rate):
     try:
         msg = MIMEText(f"Alert: Device {device_ip} is behaving abnormal. Current data rate : {current_rate:.2f} KB/s")
-        msg['Sunject'] = f"IPS Alert - Abnormal traffic detected on {device_ip}"
+        msg['Subject'] = f"IPS Alert - Abnormal traffic detected on {device_ip}"
         msg['From'] = alert_email
         msg["To"] = alert_email
         
         server = smtplib.SMTP('smtp.gmail.com' , 587)
         server.starttls()
-        server.login(alert_email, "KTy@2022")
+        server.login(alert_email, "APP_PASSWORDS")
         server.send_message(msg)
         server.quit()
         print(f"Alert email sent for {device_ip}")
@@ -54,3 +54,43 @@ def log_traffic(device_mac):
     print(f"Traffic logged: {filepath}")
     return filepath        
           
+          
+          
+def monitor_device(device_ip, device_mac, max_rate, n_minutes=5):
+    print(f"Monitoring {device_ip} (max: {max_rate} KB/s) ...")
+    
+    while monitoring:
+        current_rate = get_bandwitdh(device_ip)
+        print(f"{device_ip}: {current_rate:.2f} KB/s")
+        
+        if current_rate > max_rate:
+            print(f"Alert : Abnormal traffic on {device_ip}")
+            log_traffic(device_mac)
+            send_alert(device_ip, current_rate)
+            
+            time.sleep(1)
+            
+def start_monitoring(device_ip, device_mac, max_rate, n_minutes=5):
+    global monitoring
+    monitoring = True
+    
+    thread = threading.Thread(target=monitor_device, args=(device_ip, device_mac, max_rate, n_minutes))
+    thread.daemon = True
+    thread.start()
+    print(f"Monitoring started for {device_ip}")
+
+def stop_monitoring():
+    global monitoring
+    monitoring = False
+    print("Monitoring stopped") 
+    
+if __name__ == "__main__":
+    start_monitoring("172.20.10.1", "16:2d:4d:de:e7:64", max_rate=100)
+    
+    try:
+        time.sleep(10)
+    except KeyboardInterrupt:
+        pass
+    
+    stop_monitoring()
+    print("Test finished")               
